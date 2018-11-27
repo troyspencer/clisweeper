276699281a2b7a7bfa5c85e28b5086cbd1f23690
troyspencer <troy.a.spencer@gmail.com>
2018-11-26 23:11:49 -0600

    Added -s size CLI flag for setting s x s dimensions of grid, s defaults to 10 Added -b bombs CLI flag for setting number of bombs, b defaults to 20 (currently just sets a config, doesn't actually load bombs)

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ require (
 	github.com/JoelOtter/termloop v0.0.0-20181112204055-0f8867e43cbb
 	github.com/mattn/go-runewidth v0.0.3 // indirect
 	github.com/nsf/termbox-go v0.0.0-20181027232701-60ab7e3d12ed // indirect
+	github.com/urfave/cli v1.20.0
 )
--- main.go
+++ main.go
@@ -1,25 +1,75 @@
 package main
 
-import "github.com/JoelOtter/termloop"
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/JoelOtter/termloop"
+	"github.com/urfave/cli"
+)
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "clisweeper"
+	app.Usage = "play minesweeper with CLI"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "bombs, b",
+			Usage: "Creates a minefield with b bombs, defaults to 20",
+		},
+		cli.StringFlag{
+			Name:  "size, s",
+			Usage: "Creates a minefield of dimensions size x size, defaults to 10",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		config := Configuration{}
+		config.tilesH = 10
+		config.tilesW = 10
+		config.bombs = 20
+
+		if c.String("size") != "" {
+			num, err := strconv.Atoi(c.String("size"))
+			if err != nil {
+				log.Fatal()
+			}
+			config.tilesH, config.tilesW = num, num
+		}
+		if c.String("bombs") != "" {
+			num, err := strconv.Atoi(c.String("bombs"))
+			if err != nil {
+				log.Fatal()
+			}
+			config.bombs = num
+		}
+
+		playGame(config)
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func playGame(config Configuration) {
 	game := termloop.NewGame()
 	level := termloop.NewBaseLevel(termloop.Cell{
 		Bg: termloop.ColorDefault,
 	})
 
-	tilesW := 10
-	tilesH := 10
 	tileSize := 1
 	tileWidth := tileSize * 2
 	tileHeight := tileSize
 
 	// Create enough space for tiles surrounded by space
-	level.AddEntity(termloop.NewRectangle(0, 0, (tilesW)*tileWidth*2+tileWidth, (tilesH)*tileHeight*2+tileHeight, termloop.ColorBlue))
+	level.AddEntity(termloop.NewRectangle(0, 0, (config.tilesW)*tileWidth*2+tileWidth, (config.tilesH)*tileHeight*2+tileHeight, termloop.ColorBlue))
 
 	// create tiles
-	for tileX := 0; tileX < tilesW; tileX++ {
-		for tileY := 0; tileY < tilesH; tileY++ {
+	for tileX := 0; tileX < config.tilesW; tileX++ {
+		for tileY := 0; tileY < config.tilesH; tileY++ {
 			// create tile
 			tile := Tile{
 				Entity: termloop.NewEntity(2*tileWidth*(tileX)+tileWidth, 2*tileHeight*(tileY)+tileHeight, tileWidth, tileHeight),
@@ -37,6 +87,12 @@ func main() {
 
 	game.Screen().SetLevel(level)
 	game.Start()
+}
+
+type Configuration struct {
+	tilesW int
+	tilesH int
+	bombs  int
 }
 
 // Tile is an entity that will be drawn with space on all sides in a grid
